e7d6905e4a991f957da300b51e76fdc24bdcaf2c
Yukiowtnb <124426799+Yukiowtnb@users.noreply.github.com>
2023-12-16 11:08:00 +0300

    lab_2
    +

--- Task02/guess-number/src/Controller.php
+++ Task02/guess-number/src/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Yukiowtnb\GuessNumber\Controller;
-
+use function Yukiowtnb\GuessNumber\View\showGame;
 use function cli\line;
 
 function startGame()
---
+++ Task02/guess-number/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yukio/guess-number',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c1316b0eef332b0a14d79d0212455428206e3c05',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'reference' => '3c2385497f806decca1e5abeba3cb8fd7caba4e0',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => true,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.22',
+            'version' => '0.11.22.0',
+            'reference' => 'a6bb94664ca36d0962f9c2ff25591c315a550c51',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yukio/guess-number' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c1316b0eef332b0a14d79d0212455428206e3c05',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
